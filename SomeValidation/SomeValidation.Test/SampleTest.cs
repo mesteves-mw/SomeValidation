420e339702a72083e66b0643c7252888fd7de383
mesteves <mesteves@cayan.com>
2018-04-08 23:54:35 +0100

    0.1.0: Simplified Validationframework event driven. Gives the consumer the option to handle errors and log them. Consumer can also choose to handle how property names nesting occurs by managing property name chaining state.

---
+++ SomeValidation/SomeValidation.Test/SampleTest.cs
@@ -0,0 +1,14 @@
+﻿namespace SomeValidation.Test
+{
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class SampleTest
+    {
+        [Test]
+        public void Test()
+        {
+            
+        }
+    }
+}
