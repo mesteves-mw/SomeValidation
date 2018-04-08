aaede33d34360906b7ad5f14cb0fabf6024f926f
mesteves <mesteves@cayan.com>
2018-04-09 00:01:39 +0100

    0.1.0: Property chaining is now abstracted from the consumer by managing a ClassParameterName state on each validator. Setback: This will cause the entry validator instance to not be thread-safe i.e. the ClassParameterName cant be changed.

--- SomeValidation/SomeValidation.Test/SampleTest.cs
+++ SomeValidation/SomeValidation.Test/SampleTest.cs
@@ -3,12 +3,73 @@
     using NUnit.Framework;
 
     [TestFixture]
-    public class SampleTest
+    public class ValidatorTest
     {
+        public class Customer
+        {
+            public string Name { get; set; }
+            public Address AddressData { get; set; }
+            public int Age { get; set; }
+        }
+
+        public class Address
+        {
+            public string PostCode { get; set; }
+            public string Street { get; set; }
+            public Customer Owner { get; set; }
+        }
+
+        public class CustomerValidator : AbstractValidator<Customer>
+        {
+
+            public override void Validate(Customer c)
+            {
+                if (c.Name == null) RaiseError("Name", "{0} is null!");
+
+                Create<AddressValidator>().Validate(c.AddressData, "AddressData");
+
+                if (c.Age == 0) RaiseError("Age", "{0} is 0!");
+            }
+        }
+
+        public class AddressValidator : AbstractValidator<Address>
+        {
+
+            public override void Validate(Address a)
+            {
+                if (a == null) { RaiseError("Address", "{0} is null!"); return; }
+
+                if (a.PostCode == null) RaiseError("PostCode", "{0} is null!");
+
+                Create<CustomerValidator>().Validate(a.Owner, "Owner");
+
+                if (a.Street == null) RaiseError("Street", "{0} is null!");
+            }
+        }
+
         [Test]
         public void Test()
         {
-            
+            var cust = new Customer();
+            cust.AddressData = new Address();
+            cust.AddressData.Owner = new Customer();
+
+            var cv = new CustomerValidator();
+
+            string errors = "";
+
+            cv.OnError += (p, e) => errors += string.Format(" -- " + e + "\n", p);
+
+            cv.Validate(cust, "cust");
+
+            Assert.That(errors, Is.EqualTo(
+                " -- cust.Name is null!\n" +
+                " -- cust.AddressData.PostCode is null!\n" +
+                " -- cust.AddressData.Owner.Name is null!\n" +
+                " -- cust.AddressData.Owner.AddressData.Address is null!\n" +
+                " -- cust.AddressData.Owner.Age is 0!\n" +
+                " -- cust.AddressData.Street is null!\n" +
+                " -- cust.Age is 0!\n"));
         }
     }
 }
---
+++ SomeValidation/SomeValidation/AbstractValidator.cs
@@ -0,0 +1,45 @@
+﻿namespace SomeValidation
+{
+    using System;
+
+    public abstract class AbstractValidator
+    {
+        public string ClassParameterName { get; set; }
+
+        public event Action<string, string> OnError;
+
+        public T Create<T>() where T : AbstractValidator, new()
+        {
+            var t = new T();
+            t.ClassParameterName = this.ClassParameterName;
+
+            t.OnError = (a, b) => OnError(a, b);
+
+            return t;
+        }
+
+        public void RaiseError(string propertyName, string message)
+        {
+            OnError(this.ClassParameterName + "." + propertyName, message);
+        }
+    }
+
+    public abstract class AbstractValidator<T> : AbstractValidator
+    {
+        public void Validate(T t, string pname)
+        {
+            if (string.IsNullOrEmpty(this.ClassParameterName))
+            {
+                this.ClassParameterName = pname;
+            }
+            else
+            {
+                this.ClassParameterName += "." + pname;
+            }
+
+            this.Validate(t);
+        }
+
+        public abstract void Validate(T t);
+    }
+}
